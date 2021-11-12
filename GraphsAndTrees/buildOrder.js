6ba8505b2513cc8f07fa0d513aab25d096610ada
Nicole <nicole.pan1127@gmail.com>
2021-11-12 11:33:14 -0500

    topological sort

--- GraphsAndTrees/buildOrder.js
+++ GraphsAndTrees/buildOrder.js
@@ -1,26 +1,76 @@
 function buildOrder(projects, dependencies){
-  let index = {}
-  let numberOfDependencies = Array(projects.length).fill(0)
-  let adjacencyMatrix = []
-  for(let i = 0 ; i < projects.length; i ++){
-    index[projects[i]] = i
-    adjacencyMatrix.push(Array(projects.length).fill(0))
-  }
-  for(let j = 0; j < dependencies.length; j++){
-    let p1 = index[dependencies[j][0]]
-    let p2 = index[dependencies[j][1]]
-    adjacencyMatrix[p1][p2] = 1
-    numberOfDependencies[p2] += 1
-  }
-  console.log(numberOfDependencies)
-  console.log(adjacencyMatrix)
-
-  while(numberOfDependencies.includes(0)){
-    let canComplete = numberOfDependencies.findIndex( (ele) => ele === 0)
-    for( let i in adjacencyMatrix[canComplete]){
-      console.log(adjacencyMatrix[canComplete][i])
+  let graph = new Graph([...projects], [...dependencies])
+  let order = []
+  while(graph.nodesWithNoDependencies()){
+    let nodeToRemove = graph.nodesWithNoDependencies()
+    if(!nodeToRemove){
+      return "No Build Order Possible"
     }
-    numberOfDependencies.splice(canComplete, 1)
+    if(graph.incomingEdges(nodeToRemove)){
+      let edgesToRemove = [...graph.incomingEdges(nodeToRemove)]
+      for(let i = 0; i < edgesToRemove.length; i++){
+        graph.removeEdge(edgesToRemove[i] ,nodeToRemove)
+      }
+    }
+    order.push(nodeToRemove)
+    graph.deleteNode(nodeToRemove)
+  }
+  return (order.length == projects.length) ? order : "No Build Order Possible"
+}
+
+class Graph {
+  constructor(nodes, dependencies){
+    this.nodes = nodes
+    this.outgoing = {}
+    this.incoming = {}
+    for (let i in dependencies){
+      let key = dependencies[i][1]
+      let value = dependencies[i][0]
+      if(this.outgoing[key]){
+        this.outgoing[key] = [... this.outgoing[key], value]
+      } else{
+        this.outgoing[key] = [value]
+      }
+
+      if(this.incoming[value]){
+        this.incoming[value] = [... this.incoming[value], key]
+      } else{
+        this.incoming[value] = [key]
+      }
+    }
+  }
+
+
+  outgoingEdges = (node) => {
+    return this.outgoing[node]
+  }
+
+  incomingEdges = (node) => {
+    return this.incoming[node]
+  }
+
+  deleteNode = (node) => {
+    delete this.outgoing[node]
+    if(this.nodes.includes(node)){
+      this.nodes.splice(this.nodes.findIndex( ele => ele == node), 1)
+    }
+  }
+
+  removeEdge(nodeA, nodeB){
+    this.incoming[nodeB] = this.incoming[nodeB].filter (ele => ele != nodeA)
+    this.outgoing[nodeA] = this.outgoing[nodeA].filter (ele => ele != nodeB)
+  }
+
+  nodesWithNoDependencies() {
+    for(let node in this.nodes){
+      if(!this.outgoing[this.nodes[node]]){
+        return this.nodes[node]
+      }
+      if(this.outgoing[this.nodes[node]].length == 0){
+        return this.nodes[node]
+      }
+    }
+    return false
   }
 }
 
